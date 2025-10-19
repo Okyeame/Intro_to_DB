62d98588c3a0d036be8a79332babec98d255c95c
Samuel Anokye <andrewssamuelanokye@gmail.com>
2025-10-20 00:07:21 +0100

    newfile

--- task_6.sql
+++ task_6.sql
@@ -1,47 +1,9 @@
--- Create the database
-CREATE DATABASE IF NOT EXISTS alx_book_store;
-
--- Use the database
+-- Select the database
 USE alx_book_store;
 
--- Create Authors table
-CREATE TABLE AUTHORS (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
-);
-
--- Create Books table
-CREATE TABLE BOOKS (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
-    author_id INT NOT NULL,
-    price DOUBLE NOT NULL,
-    publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
-);
-
--- Create Customers table
-CREATE TABLE CUSTOMERS (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL,
-    address TEXT
-);
-
--- Create Orders table
-CREATE TABLE ORDERS (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT NOT NULL,
-    order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
-);
-
--- Create Order_Details table
-CREATE TABLE ORDER_DETAILS (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT NOT NULL,
-    book_id INT NOT NULL,
-    quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
-);
+-- Insert multiple rows into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES 
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
