3609ec0b9c45ef3038fc39063817b1fb448ee89f
Samuel Anokye <andrewssamuelanokye@gmail.com>
2025-10-20 00:01:08 +0100

    bugfixes

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,8 @@
 -- Select the database
 USE alx_book_store;
 
--- Show the full description of the table 'books'
-SHOW CREATE TABLE books;
+-- Print the full description of the table 'Books' using INFORMATION_SCHEMA
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
