73ef2f9516ceba53e00d544bab29ad6198bebf2a
Samuel Anokye <andrewssamuelanokye@gmail.com>
2025-10-19 23:48:58 +0100

    newfile

---
+++ task_2.sql
@@ -0,0 +1,20 @@
+-- Use the existing database
+USE alx_book_store;
+
+-- Create Authors table
+CREATE TABLE IF NOT EXISTS authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- Create Books table
+CREATE TABLE IF NOT EXISTS books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+-- Create Customers tabl
